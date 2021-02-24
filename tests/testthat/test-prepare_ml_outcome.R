3b89ba46e1fc992e9ffe1c8f49c99482aa403a79
ahrensmaike <maike.ahrens.ext@bayer.com>
2021-02-24 19:49:58 +0100

    unit tests regression: NA removal, outlier removal

--- tests/testthat/test-prepare_ml_outcome.R
+++ tests/testthat/test-prepare_ml_outcome.R
@@ -3,30 +3,50 @@
 
 test_that("outcome preparation works (NA removal)", {
   
-  outcome <- c(20, rnorm(9)) %>%  tibble::as_tibble_col('num_outcome')
+  set.seed(1605)
+  outcome <- c(NA,  rnorm(9)) %>%  tibble::as_tibble_col('num_outcome') %>% 
+    dplyr::mutate(.id = 1:n(), .before=1)
+  
+  prep_res <- prepare_ml_outcome(
+    outcome
+  )
   
   # NA removal
-  expect_equal(2 * 2, 4)
-  
+  testthat::expect_equal(prep_res$outcome %>%  nrow, 9)
   
 })
+
 
 # OUTLIER REMOVAL  #### 
 test_that("outcome preparation works (outlier removal for regression)", {
   
   set.seed(1605)
-  outcome <- c(20, rnorm(9)) %>%  tibble::as_tibble_col('num_outcome') %>% 
+  outcome <- c(20,  rnorm(9)) %>%  tibble::as_tibble_col('num_outcome') %>% 
     dplyr::mutate(.id = 1:n(), .before=1)
   
-  prepare_ml_outcome(
-    
+  prep_res <- prepare_ml_outcome(
     outcome,
-    outcome_name   = NULL,
-    level_order    = NULL,
-    outlier_remove = FALSE,
+    outlier_remove = TRUE,
     outlier_ctrl   = list(coef = 3)
-    
   )
-    
   
+  testthat::expect_equal(
+    prep_res$id_outlier,
+    1
+  )
+  
+  testthat::expect_equal(
+    nrow(prep_res$outcome),
+    9
+  )
+  
+  testthat::expect_length(
+    prepare_ml_outcome(
+      outcome,
+      outlier_remove = TRUE,
+      outlier_ctrl   = list(coef = 10)
+    )$id_outlier,
+    0
+  )
 })
+
