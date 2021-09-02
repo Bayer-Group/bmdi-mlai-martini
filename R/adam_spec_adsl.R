abb5b9a38cab715cf8d490dd963527bb5736e6b4
ahrensmaike <maike.ahrens.ext@bayer.com>
2021-09-02 09:04:25 +0000

    fix typo

--- R/adam_spec_adsl.R
+++ R/adam_spec_adsl.R
@@ -260,7 +260,7 @@
   
   if("RANDDT" %in% colnames(adsl)){
     # also check it's not constant (e.g. all NA in IA)
-    if(d_sl %>%  pull(RANDDT) %>% n_distinct() %>%  {.>1}){
+    if(adsl %>%  pull(RANDDT) %>% n_distinct() %>%  {.>1}){
     
     adsl_cor_randdt <- adsl %>%
       dplyr::select(-tidyselect::all_of(all_FL)) %>% 
