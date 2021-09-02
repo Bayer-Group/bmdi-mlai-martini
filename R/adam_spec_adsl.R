a247b035608107063ad873055ad391974824e8d9
ahrensmaike <maike.ahrens.ext@bayer.com>
2021-09-02 09:04:25 +0000

    add safety condition  non constant cors_randdt

--- R/adam_spec_adsl.R
+++ R/adam_spec_adsl.R
@@ -283,8 +283,11 @@
   
   # potential remaining numeric id columns are identified by monotonous relation with randomization date
   cors_randdt <- NULL
-  if ("RANDDT" %in% colnames(adsl)){
-    
+
+  if("RANDDT" %in% colnames(adsl)){
+    # also check it's not constant (e.g. all NA in IA)
+    if(adsl %>%  pull(RANDDT) %>% n_distinct() %>%  {.>1}){
+
     adsl_cor_randdt <- adsl %>%
       dplyr::select(-tidyselect::all_of(all_FL)) %>% 
       dplyr::mutate(RANDDT = as.Date(RANDDT) %>% as.numeric()) %>% 
