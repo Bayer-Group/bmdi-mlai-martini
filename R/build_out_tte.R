ca2e750940af26b58feb60ca3444faba8816abfa
ahrensmaike <maike.ahrens.ext@bayer.com>
2022-01-11 18:01:12 +0000

    init build_out_tte() incl test

--- R/build_out_tte.R
+++ R/build_out_tte.R
@@ -3,11 +3,11 @@
 #' @param data adtte
 #' @param file path to adam-like tte data set, e.g. files/adtte.sas7bdat, ignored if data is provided
 #' @param filter character vector to be applied to the data set (e.g. to select PARAMCD or subset to particular population)
-#' @param cut optional, create binarized version from tte data
+#' @param cut optional numeric, required for binarized version from tte data. Note that unit must provided in the same unit as the time column (e.g. AVAL)
 #' @param unit only relevant if outcome should be binarized, i.e. if `cut` is provided. 
 #' `unit` should be original unit of numeric (time) column in data set, to be included in final label (event/no event after `cut unit`(s)).
 #' @param columns list defining the mapping of required information to columns in the data with default entries. See details for defaults. 
-#' 
+#' @param label optional for the binarized outcome variable, defaults to '.out'
 #' 
 #' @details 
 #' Column mapping defaults are chosen to match adam adtte data sets: 
@@ -30,112 +30,69 @@ build_out_tte <- function(
   cut     = NULL,
   unit    = 'unit',
   columns = NULL
-  # NOTE further columns to be added for binarization
-  
 ){
  
   columns_used <- list(
     id     = 'SUBJID',
     time   = 'AVAL',
     censor = 'CNSR',
-    status = NULL, 
-    LVDT   = 'LVDT'
+    status = NULL
   )
   
   # input checks
-  if (all(c(is.null(data), is.null(file)))){
+  if(all(c(is.null(data), is.null(file)))){
     usethis::ui_stop(paste0('At least one of ', usethis::ui_code('data'), ' or ', usethis::ui_code('file'), ' need to be provided.'))
   }
   
+  if(!is.numeric(cut)){
+    usethis::ui_stop(paste0(usethis::ui_code('cut'), ' needs to be numeric.'))
+  }
+  
+  
+  
   #  data prep
-  if (!is.null(data)){
+  if (is.null(data)){
     data <- haven::read_sas(file)
   }
   
   columns_used <- purrr::list_modify(columns_used, !!!columns)
   
-  tte <- data %>% 
+  data %>% 
     dplyr::filter(!!! rlang::parse_exprs(filter)) %>% 
     {if(!is.null(columns_used$status)){
-      dplyr::mutate(., .status = !!rlang::sym(columns_used$status))
+      dplyr::mutate(., .status =     !!rlang::sym(columns_used$status))
     }else{
       dplyr::mutate(., .status = 1 - !!rlang::sym(columns_used$censor))
-    }} 
+    }} %>% 
+    dplyr::rename(
+      .id   = !!rlang::sym(columns_used$id),
+      .time = !!rlang::sym(columns_used$time)
+    ) %>% 
+
+    
+    { if(!is.null(cut)){ 
+        dplyr::mutate(.,
+         .out = case_when(
+            # censored within cut -> drop
+            .time <= cut & .status == 0 ~ NA_character_,
+            .time <= cut & .status == 1 ~ paste0("event",    " in first ", cut, " ", unit, "(s)"),
+            .time >  cut                ~ paste0("no event", " in first ", cut, " ", unit, "(s)")
+          )) %>% 
+        dplyr::filter(!is.na(.out)) %>% 
+        dplyr::select(-.time, -.status)
+      }else{.}
+    } %>% 
+    
+    dplyr::select(tidyselect::any_of(c('.id', '.out', '.time', '.status'))) %>% 
   
-  if (is.null(cut)){
-    
-    tte %>% 
-      dplyr::select(
-        .id   = !!rlang::sym(columns_used$id),
-        .time = !!rlang::sym(columns_used$time),
-        .status
-      )
-    
-  } else {
-    
-  }
-   
+    labelled::set_variable_labels(
+      .,
+      .labels = list(
+        .out = ifelse(is.null(label), '.out', label),
+        .status = 'event indicator'
+      ),
+      .strict = FALSE
+    )
+  
 }
-
-
-### Prepare
-
-# ```{r}
-# # date of last visit
-# vsmax <- d_out$LVDT %>% as.Date %>% max
-# 
-# d_out_cat <- d_out %>% 
-#   mutate(EVENT = case_when(
-#     CNSR == 1 ~ "no event",
-#     CNSR == 0 ~ "event"
-#   )) %>% 
-#   mutate(EV36M = case_when(
-#     AVAL <= 36 & CNSR == 1 ~ "censored in first 36 months",
-#     AVAL <= 36 & CNSR == 0 ~ "event in first 36 months",
-#     AVAL >  36             ~ "no event in first 36 months"
-#   )) %>% 
-#   # potential full years at risk
-#   mutate(FYATRISK = floor(interval(as.Date(STARTDT), vsmax)/years(1))) %>% 
-#   mutate_at("EVENT", factor) %>% 
-#   mutate_at(c("EV36M"), ~{factor(.) %>% fct_shift(., n = 1)}) %>% 
-#   select(any_of(c("SUBJID", "FYATRISK", "EVENT", "EV36M")))
-# 
-# 
-# var_label(d_out_cat) <- list(
-#   FYATRISK  = "Number of full years until last visit",
-#   EVENT     = "CV death or non-fatal CV event (yes/no)",
-#   EV36M     = "CV death or non-fatal CV event in first 36 months (yes/no)"
-# )
-# 
-# outcome_lab <- var_label(d_out_cat) %>% 
-#   unlist() %>% 
-#   enframe(name = "PARAM", value = "OUTCOME")
-# 
-# tbl_out <- d_out_cat %>% 
-#   pivot_longer(-any_of(c("SUBJID", "FYATRISK")), names_to = "PARAM", values_to = "CLASS") %>% 
-#   count(PARAM, CLASS) %>% 
-#   right_join(outcome_lab, ., by = "PARAM") %>% 
-#   add_count(OUTCOME, name = "N", wt = n) %>% 
-#   mutate(prop = round(n/N, 3)) %>% 
-#   select(-PARAM, -N)
-# 
-# tbl_out %>% 
-#   kable() %>% 
-#   kable_styling(bootstrap_options = c("hover", "responsive"), font_size = 13, full_width = FALSE, position = "left") %>% 
-#   column_spec(1, bold = TRUE) %>% 
-#   collapse_rows(1, valign = "top")
-# ```
-# 
-# ***
-#   
-#   ```{r}
-# d_out_cat <- d_out_cat %>% 
-#   rename(.id = SUBJID) %>% 
-#   mutate(EV36M = as.character(EV36M)) %>% 
-#   mutate(EV36M = if_else(EV36M == "censored in first 36 months", NA_character_, EV36M) %>% factor()) %>% 
-#   droplevels() %>% 
-#   select(-FYATRISK)
-# 
-# var_label(d_out_cat)[c("EVENT", "EV36M")] <- outcome_lab %>% deframe() %>% .[c("EVENT", "EV36M")]
-# ```
 
--- tests/testthat/test-build_out_tte.R
+++ tests/testthat/test-build_out_tte.R
@@ -1,3 +1,36 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("build_out_tte works", {
+  
+  cut  <- 20
+  unit <- 'days'
+  data <- tribble(
+    ~SUBJID, ~AVAL, ~CNSR,
+      10001, 10,    0,
+      10002, 30,    0,
+      10003, 10,    1,
+      10004, 40,    1
+  ) %>% 
+    mutate(PARAMCD = 'A') %>% 
+    bind_rows(
+      .,
+      mutate(., PARAMCD = 'B') %>% mutate(CNSR = 1 - CNSR)
+    )
+  
+  # discard subjects censored before cut
+  testthat::expect_true(
+    !
+    #data %>% 
+    #  filter(AVAL < cut & CNSR == 1) %>% 
+    #  pull(SUBJID)
+    '10003'
+    
+    %in%
+    
+    build_out_tte(
+      data = data, 
+      filter = 'PARAMCD == "A"', 
+      cut = cut
+    )$.id
+  
+  )
+  
 })
