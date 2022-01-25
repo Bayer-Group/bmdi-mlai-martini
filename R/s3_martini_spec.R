06f103eaf60ac971008245736559581312c0ee54
svoss09 <sebastian.voss.ext@bayer.com>
2022-01-25 17:45:25 +0000

    print method martini_spec

---
+++ R/s3_martini_spec.R
@@ -0,0 +1,16 @@
+
+
+print.martini_spec <- function(x){
+  
+  purrr::imap_dfr(x, ~{
+    tibble::tibble(.y, .x$type, .x$size)
+  }) %>% 
+    purrr::map_dfr(~{
+      stringr::str_pad(.x, width = max(nchar(.x)), side = "right")
+    }) %>% 
+    tidyr::unite(col = "x", everything(), sep = " ") %>% 
+    pull(x) %>% 
+    paste(collapse = "\n") %>% 
+    cat()
+  
+}
