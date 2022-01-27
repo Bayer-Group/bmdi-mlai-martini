a8ea3a85f59f256df679878fae9fc439b0597d0d
svoss09 <sebastian.voss.ext@bayer.com>
2022-01-27 17:07:00 +0000

    print method update (work in progress)

--- R/s3_martini_spec.R
+++ R/s3_martini_spec.R
@@ -3,7 +3,7 @@
 print.martini_spec <- function(x){
   
   purrr::imap_dfr(x, ~{
-    tibble::tibble(.y, .x$type, .x$size)
+    tibble::tibble(.y, .x$type, as.character(.x$size))
   }) %>% 
     purrr::map_dfr(~{
       stringr::str_pad(.x, width = max(nchar(.x)), side = "right")
