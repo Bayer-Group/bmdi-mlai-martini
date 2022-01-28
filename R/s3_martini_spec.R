1642cbb54afcd6f548712910a3b31d97b0292048
svoss09 <sebastian.voss.ext@bayer.com>
2022-01-28 16:26:37 +0000

    added number of extracted variables to print method of adam_spec()

--- R/s3_martini_spec.R
+++ R/s3_martini_spec.R
@@ -2,21 +2,40 @@
 
 print.martini_spec <- function(x){
   
-  txt_sum <- c("name", "type", "size") %>% 
+  txt_sum <- c("name", "type", "size", "ncol") %>% 
     rlang::set_names() %>% 
     tibble::as_tibble_row() %>% 
     dplyr::bind_rows(
       purrr::imap_dfr(x, ~{
+        
+        # determine number of resulting COL number in wide data set by number of (selected) ROWS from dict
+        ncol <- .x$dict %>%  
+          {if('selected' %in% names(.)){
+            dplyr::filter(., selected)
+          }else{.} 
+          } %>% 
+          nrow()
+        
         tibble::tibble(
           name = .y, 
           type = .x$type,  
-          size = ifelse(is.null(.x$size), NA_character_, as.character(.x$size))
+          size = ifelse(
+            is.null(.x$size), 
+            NA_character_, 
+            .x$size %>% fs::as_fs_bytes() %>% as.character()),
+          ncol = as.character(ncol)
         )
       })
     ) %>% 
-    purrr::map_dfr(~{
-      stringr::str_pad(.x, width = max(nchar(.x), na.rm = TRUE), side = "right")
-    }) %>% 
+    dplyr::mutate_at(
+      c('name', 'type'), 
+      ~crayon::col_align(.x, align = 'left' , width = max(nchar(.x), na.rm = TRUE))
+    ) %>% 
+    dplyr::mutate_at(
+      c('size', 'ncol'),
+      ~crayon::col_align(.x, align = 'right', width = max(nchar(.x), na.rm = TRUE))
+    ) %>% 
+    
     tidyr::unite(col = "x", tidyselect::everything(), sep = " ") %>% 
     dplyr::pull(x) %>% 
     paste('  ', ., '\n')
