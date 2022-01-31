de18ff1e134e720a8f4d0a351671e6cd00fad6f2
ahrensmaike <maike.ahrens.ext@bayer.com>
2022-01-31 17:10:46 +0000

    get_data() init

---
+++ R/get_data.R
@@ -0,0 +1,31 @@
+#' Extract data from an ml object
+#' 
+#' Combine splitted data (training and test, if available) from either prepared or raw data
+#'
+#' @param dml ml object as returned by \code{\link{prepare_ml}()}
+#' @param type either 'prep' or 'raw', determining which state of the data should be extracted. Defaults to `prep`.
+#' @param split_id Add column indicating split origin (train/test). Omitted if NULL (default)
+#'
+#' @return 
+#' result of `bind_rows` of data sets in `dml` of the chosen type, either with or without an added `train_test`column
+#'
+#' @section Authors:
+#' Maike Ahrens (ahrensmaike), Sebastian Voss (svoss09)
+#' 
+#' @export
+
+get_data <- function(
+  dml, 
+  type = c('prep', 'raw'),
+  split_id = NULL
+  ){
+  
+  d_type <- dml[[paste0('data_', type)]]
+  
+  bind_rows(
+    train = d_type$train, 
+    test  = d_type$test,  # is NULL if no split was done
+    .id   = split_id
+  )
+  
+}
