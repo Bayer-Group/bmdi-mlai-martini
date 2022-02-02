799dbb065f6c81b068f164f86a5a99a25b1c88e0
svoss09 <sebastian.voss.ext@bayer.com>
2022-02-02 19:14:32 +0000

    updated print method for adam_spec() - key columns in bds/occds

--- R/s3_martini_spec.R
+++ R/s3_martini_spec.R
@@ -2,6 +2,12 @@
 
 print.martini_spec <- function(x){
   
+  txt_print <- c(
+    "\n",
+    crayon::silver("  Content"),
+    "\n"
+  )
+  
   txt_sum <- c("name", "type", "size", "ncol") %>% 
     rlang::set_names() %>% 
     tibble::as_tibble_row() %>% 
@@ -38,10 +44,84 @@ print.martini_spec <- function(x){
     
     tidyr::unite(col = "x", tidyselect::everything(), sep = " ") %>% 
     dplyr::pull(x) %>% 
-    paste('  ', ., '\n')
+    paste0('  ', ., '\n')
   
   txt_sum[1] <- crayon::bold(txt_sum[1]) %>% crayon::blue()
   
-  purrr::walk(txt_sum, cat)
+  txt_print <- c(txt_print, txt_sum)
+  
+  # key columns ####
+  
+  types     <- purrr::map_chr(x, "type") 
+  
+  # ... bds ####
+  
+  names_bds <- names(types)[which(types == 'bds')] %>% rlang::set_names()
+  
+  if(any( purrr::map_chr(x, "type")== "bds")){
+
+    bds_keys  <- c("param", "value", "unit", "time")
+    
+    txt_bds <- c("name", bds_keys) %>% 
+      rlang::set_names() %>% 
+      tibble::as_tibble_row() %>% 
+      dplyr::bind_rows(
+        purrr::imap_dfr(names_bds, ~{c(name = .y, x[[.x]][bds_keys])})
+      ) %>% 
+      dplyr::mutate_all(
+        ~crayon::col_align(.x, align = 'left' , width = max(nchar(.x), na.rm = TRUE))
+      ) %>% 
+      tidyr::unite(col = "x", tidyselect::everything(), sep = " ") %>% 
+      dplyr::pull(x) %>% 
+      paste0('  ', ., '\n')
+    
+    txt_bds[1] <- crayon::bold(txt_bds[1]) %>% crayon::blue()
+    
+    txt_bds <- c(
+      "\n",
+      crayon::silver("  Key columns used in bds-type data sets"),
+      "\n",
+      txt_bds
+    )
+    
+    txt_print <- c(txt_print, txt_bds)
+    
+  }
+  
+  # ... occds ####
+  
+  names_occds <- names(types)[which(types == 'occds')] %>% rlang::set_names()
+  
+  if(any( purrr::map_chr(x, "type")== "occds")){
+    
+    occds_keys  <- c("label", "value", "valuen", "count", "time")
+    
+    txt_occds <- c("name", occds_keys) %>% 
+      rlang::set_names() %>% 
+      tibble::as_tibble_row() %>% 
+      dplyr::bind_rows(
+        purrr::imap_dfr(names_occds, ~{c(name = .y, x[[.x]][occds_keys] %>% unlist())})
+      ) %>% 
+      dplyr::mutate_all(
+        ~crayon::col_align(.x, align = 'left' , width = max(nchar(.x), na.rm = TRUE))
+      ) %>% 
+      tidyr::unite(col = "x", tidyselect::everything(), sep = " ") %>% 
+      dplyr::pull(x) %>% 
+      paste0('  ', ., '\n')
+    
+    txt_occds[1] <- crayon::bold(txt_occds[1]) %>% crayon::blue()
+    
+    txt_occds <- c(
+      "\n",
+      crayon::silver("  Key columns used in occds-type data sets"),
+      "\n",
+      txt_occds
+    )
+    
+    txt_print <- c(txt_print, txt_occds)
+    
+  }
+  
+  purrr::walk(txt_print, cat)
   
 }
