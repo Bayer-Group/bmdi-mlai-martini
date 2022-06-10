2c08990fcf542c37db9e76c6822db3e2f4bc6eb3
svoss09 <sebastian.voss.ext@bayer.com>
2022-06-10 16:13:02 +0000

    add test for dict update after adjust_adsl

--- tests/testthat/test-adjust_adsl.R
+++ tests/testthat/test-adjust_adsl.R
@@ -7,17 +7,31 @@ test_that("adjust_adsl", {
   add  <- "AGEGR01"
   drop <- "AGE"
   
-  select_adj <- martini_spec %>% 
+  martini_spec_adj <- martini_spec %>% 
     adjust_adsl(
       add  = add,
       drop = drop
-    ) %>% 
+    ) 
+  
+  select_adj <- martini_spec_adj %>% 
     .[["adsl"]] %>% 
     .[["select"]]
   
+  # check that spec's select slot is updated
   expect_setequal(
     c(select_orig, add) %>% unique() %>% setdiff(drop),
     select_adj
+  )
+  
+  # check that dict column is updated
+  expect_true(
+    martini_spec_adj$adsl$dict %>%
+      dplyr::select(param, selected) %>% 
+      dplyr::filter(param %in% c(add, drop)) %>% 
+      dplyr::mutate(expect = ifelse(param %in% add, TRUE, FALSE)) %>% 
+      dplyr::mutate(match = (selected == expect)) %>% 
+      dplyr::pull(match) %>% 
+      all()
   )
   
 })
