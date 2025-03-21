cfa7794a1f6ee71cd3441838de821b80a7329e85
svoss09 <sebastian.voss.ext@bayer.com>
2025-03-21 16:51:05 +0000

    consolidate data documentation

---
+++ R/data-martini_ml.R
@@ -0,0 +1,42 @@
+#' Example ML classification object.
+#' 
+#' ML object of the example study in \code{system.file("ads", package = "martini")} and the classification
+#' outcome \code{martini_outc_class} as created by \code{\link{prepare_ml}()}.
+#' 
+#' @format 
+#' A list object. See the documentation of \code{\link{prepare_ml}()} for further details on the object structure.
+#' 
+#' @source 
+#' Simulated data
+#'
+
+"martini_ml_class"
+
+#' Example ML regression object.
+#' 
+#' ML object of the example study in \code{system.file("ads", package = "martini")} and the regression
+#' outcome \code{martini_outc_regr} as created by \code{\link{prepare_ml}()}.
+#' 
+#' @format 
+#' A list object. See the documentation of \code{\link{prepare_ml}()} for further details on the object structure.
+#' 
+#' @source 
+#' Simulated data
+#'
+
+"martini_ml_regr"
+
+
+#' Example ML survival object.
+#' 
+#' ML object of the example study in \code{system.file("ads", package = "martini")} and the survival
+#' outcome \code{martini_outc_surv} as created by \code{\link{prepare_ml}()}.
+#' 
+#' @format 
+#' A list object. See the documentation of \code{\link{prepare_ml}()} for further details on the object structure.
+#' 
+#' @source 
+#' Simulated data
+#'
+
+"martini_ml_surv"
---
+++ R/data-martini_outc.R
@@ -0,0 +1,39 @@
+#' Example classification outcome
+#'
+#' Classification outcome for the included example study in \code{system.file("ads", package = "martini")}.
+#' 
+#' @format 
+#' A tibble with one row per subject and the columns \code{.id} and \code{.out}.
+#' 
+#' @source 
+#' Simulated data
+#'
+
+"martini_outc_class"
+
+
+#' Example regression outcome
+#'
+#' Regression outcome for the included example study in \code{system.file("ads", package = "martini")}.
+#' 
+#' @format 
+#' A tibble with one row per subject and the columns \code{.id} and \code{.out}.
+#' 
+#' @source 
+#' Simulated data
+#'
+
+"martini_outc_regr"
+
+#' Example survival outcome
+#'
+#' Survival outcome for the included example study in \code{system.file("ads", package = "martini")}.
+#' 
+#' @format 
+#' A tibble with one row per subject and the columns \code{.id}, \code{.time} and \code{.status}.
+#' 
+#' @source 
+#' Simulated data
+#'
+
+"martini_outc_surv"
