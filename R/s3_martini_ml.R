f19b4905fec36293591728d9d1d3b32bd6a0351c
Maike Ahrens <maike.ahrens.ext@bayer.com>
2025-08-25 13:41:44 +0000

    WIP martini_ml print include main recipe steps

--- R/s3_martini_ml.R
+++ R/s3_martini_ml.R
@@ -11,18 +11,14 @@ print.martini_ml <- function(x, ...){
   
   # data set sizes ####
   tab_dim <- purrr::map(
-    x$data %>% purrr::set_names(~{paste0("data$", .)}), 
-    \(rec_state) purrr::map(rec_state, pillar::dim_desc) %>% tibble::as_tibble_row() 
+    x$data, 
+    \(rec_state) purrr::map(rec_state, pillar::dim_desc) %>% tibble::as_tibble_row() # 
   ) %>%
     purrr::list_rbind(names_to = "recipe_state") %>% 
     # format to char vector by row
     dplyr::add_row(
       tibble::as_tibble_row(
-        names(.) %>% 
-          purrr::set_names() %>% 
-          gsub("recipe_state", " ", .) %>% 
-          gsub("^t", "$t", .) 
-        ),
+        names(.) %>% purrr::set_names() %>% gsub("_", " ", .)),
       .) %>% 
     dplyr::mutate(recipe_state = pillar::align(recipe_state)) %>% 
     dplyr::mutate(
@@ -33,7 +29,55 @@ print.martini_ml <- function(x, ...){
     tidyr::unite(col = "x", tidyselect::everything(), sep = " ") %>% 
     dplyr::pull() %>% 
     cli::cat_line() %>% 
-    c(cli::cli_h3("Data set sizes (n {pillar:::mult_sign()} p)"), .)
+    c(cli::cli_h3("Data set sizes (n {cli::symbol$times} p)"), .)
+  
+ # main recipe steps ####
+  steps_to_report <- c(
+    "filter_missing", 
+    "log_skewness", 
+    "impute_knn", 
+    "nzv", 
+    "normalize",
+    "corr_keep", 
+    "other2", 
+    "dummy"
+    ) 
+  
+  # # version green checkmark/ red cross
+  # steps_to_report %in% 
+  #   recipes::tidy(x$recipe$prep)$type %>% 
+  #   purrr::set_names(steps_to_report) %>% 
+  #   tibble::enframe() %>% 
+  #   # for log report TRUE iff data was log transformed and NOT back transformed,
+  #   # (log_skewness && ! log_skewness_undo)
+  #   # i.e. iff prep_step_log
+  #   dplyr::mutate(value = replace(value, name == "log_skewness", x$input$args$prep_step_log)) %>%
+  #   dplyr::mutate(symbol = dplyr::if_else(value, "v", "x")) %>% 
+  #   dplyr::select(symbol, name) %>%
+  #   tibble::deframe() %>% 
+  #   cli::cli_bullets() %>% 
+  #   c(cli::cli_h3("Recipe Steps (parametrized)"), .)
+  
+  # versions with cli_symbols: checkbox on off / 
+   steps_to_report %in% 
+    recipes::tidy(x$recipe$prep)$type %>% 
+    purrr::set_names(steps_to_report) %>% 
+    tibble::enframe() %>% 
+    # for log report TRUE iff data was log transformed and NOT back transformed,
+    # (log_skewness && ! log_skewness_undo)
+    # i.e. iff prep_step_log
+    dplyr::mutate(value = replace(value, name == "log_skewness", x$input$args$prep_step_log)) %>%
+    # dplyr::mutate(symbol = dplyr::if_else(value, cli::symbol$checkbox_on, cli::symbol$checkbox_off)) %>% 
+    # dplyr::mutate(symbol = dplyr::if_else(value, cli::symbol$square_small_filled, cli::symbol$square_small)) %>% 
+    # dplyr::mutate(symbol = dplyr::if_else(value, cli::symbol$tick, cli::symbol$cross)) %>% 
+    dplyr::filter(value) %>% 
+    dplyr::mutate(symbol = dplyr::if_else(value, cli::symbol$arrow_right, cli::symbol$stop)) %>% 
+    dplyr::select(symbol, name) %>%
+    tidyr::unite(col, tidyselect::everything(), sep = " ") %>% 
+    dplyr::pull() %>%  
+    cli::cat_line() %>% 
+    c(cli::cli_h3("(parametrized) Recipe Steps"), .)
+  
   
 }
   
