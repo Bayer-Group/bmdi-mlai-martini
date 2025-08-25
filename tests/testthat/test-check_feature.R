fdd204c5abd14c075f53c74fc8df46a1bc15b792
Maike Ahrens <maike.ahrens.ext@bayer.com>
2025-08-25 15:55:00 +0000

    test check_freq() and check_feature()

---
+++ tests/testthat/test-check_feature.R
@@ -0,0 +1,31 @@
+test_that("check_feature() works", {
+  
+  # on package data: martini_feat
+  res_pkg_feat <- check_feature(martini_feat, thres_low_freq = 15)
+  
+  expect_setequal(
+    res_pkg_feat %>% names(),
+    c("low_freq", "other", "filter_missing", "count_vars")
+  )
+  
+  expect_equal(
+    res_pkg_feat,
+    list(
+      low_freq = list(
+        vars = "angina_pectoris", 
+        counts = list(
+          angina_pectoris = structure(
+            list(
+              fct = structure(1:2, levels = c("no", "yes"), class = "factor"), 
+                 n = c(275L, 14L)
+            ), 
+            class = c("tbl_df", "tbl", "data.frame"), 
+            row.names = c(NA, -2L))), 
+        overall_min = c(angina_pectoris = 14L)
+      ), 
+      other = character(0), 
+      filter_missing = character(0), 
+      count_vars = "AGE"
+    )
+  )
+})
--- tests/testthat/test-check_freq.R
+++ tests/testthat/test-check_freq.R
@@ -6,30 +6,73 @@ test_that("check_freq works", {
   
   x <- tibble::tibble(
     num       = 1:n,
-    fac_safe  = rep(c("A", "B"), each = n/2) %>% factor(),
-    fac_risky = c(rep("A", n - thres/2), rep("B", thres/2)) %>% factor()
+    fct_safe  = rep(c("A", "B"), each = n/2) %>% factor(),
+    fct_risky = c(rep("A", n - thres/2), rep("B", thres/2)) %>% factor()
   )
   
-  # case 1: no factors
-  expect_message(
-    x %>% dplyr::select(num) %>% check_freq(thres = thres),
-    "Data does not contain any factors."
+  # details on factor level frequencies are returned invisibly
+  expect_invisible(
+    check_freq(x, thres = thres, quiet = TRUE)
   )
   
-  # case 2: factors, no risk
-  expect_message(
-    x %>% dplyr::select(-fac_risky) %>% check_freq(thres = thres),
-    regexp = 'Minimum observed class size is '
+  # case 1: no factors: all empty/NA
+  expect_equal(
+   res1 <- x %>% 
+      dplyr::select(num) %>% 
+      check_freq(thres = thres),
+    list(
+      vars        = character(0),
+      counts      = structure(list(), names = character(0)), 
+      overall_min = NA_integer_
+    )
+  )
+  
+  # case 2: factors, no risk: vars & counts empty, overall min of fct_safe
+  expect_equal(
+    res2 <- x %>% 
+      dplyr::select(-fct_risky) %>%
+      check_freq(thres = thres),
+    list(
+      vars = character(0), 
+      counts = structure(list(), names = character(0)), 
+      overall_min = c(fct_safe = 50L)
+    )
   )
   
   # case 3: at least one factor with min class size below thres
-  expect_named(
-    check_freq(x, thres = thres),
-    'fac_risky'
+  expect_message(
+    res3 <- check_freq(x, thres = thres),
+    "fct_risky"
   )
   
-  # try prepare_ml output object for automated slot selection
-  expect_message(check_freq(martini_ml_regr, thres = thres))
+  expect_equal(
+    res3,
+    list(
+      vars = "fct_risky",
+      counts = list(
+        fct_risky = structure(
+          list(
+            fct = structure(1:2, levels = c("A", "B"), class = "factor"), 
+            n = c(95L, 5L)
+          ), 
+          class = c("tbl_df", "tbl", "data.frame"),
+          row.names = c(NA, -2L))
+        ), 
+      overall_min = c(fct_risky = 5L))
+  )
+  
+  # case 4: pkg data
+  # ... martini_feat 
+  expect_message(
+    res4 <- check_freq(martini_feat, thres = 50),
+    "The following factors have low frequencies"
+  )
+  # ... martini_ml output object for automated slot selection
+  expect_message(
+    res4 <- check_freq(martini_ml_class, thres = 50),
+    "The following factors have low frequencies"
+  )
+  
   
   
 })
